73ede476230871dbe207f64539c3396b374a121a
Yash Ranjan <yashranjan@ufl.edu>
2025-11-13 08:45:58 -0500

    Restructure inference code.
    Signed-off-by: Yash Ranjan <yashranjan@ufl.edu>

--- inference.py
+++ inference.py
@@ -49,11 +49,11 @@ def load_model(model_path: str):
     logger.info(f"Loading model from {model_path}")
     
     # Create mock environments (only object_type and neighbor_type are needed)
-    mock_env = Environment(data_folder=None, signal_info_folder=None, map_data_folder=map_data_folder, environment_type="Inference")
+    mock_env = Environment(data_folder=None, signal_folder=None, map_folder=map_data_folder, environment_type="Inference")
     
     # Initialize predictor
     logger.info("Initializing model...")
-    predictor = TrafficPredictor(config, mock_env, mock_env)
+    predictor = TrafficPredictor(config)
     
     # Load weights
     predictor.load_model(model_path)
@@ -70,12 +70,12 @@ class InferenceServer:
         self.sequence_length = config.get('sequence_length', 10)
         self.prediction_horizon = config.get('prediction_horizon', 5)
         self.class_map = {0: 'veh', 1: 'ped'}
-        self.center_point: Tuple[float, float] = (170.76, 296.75)
-        self.object_coordinates: Dict[str, Tuple[float, float]] = {}
+        self.center_point: Tuple[float, float] = config['center_point']
+        self.object_coordinates: Dict[int, Tuple[float, float]] = {}
         self.inference_model = inference_model
         self.timestep = 0
         self.output_distribution_type = config.get('output_distribution_type', 'linear')
-        self.trajectory_metrics = TrajectoryMetrics(dt=0.1, output_distribution_type=self.output_distribution_type, evaluation_mode=True, center_point=self.center_point)
+        self.trajectory_metrics = TrajectoryMetrics(config)
         self.radius_normalizing_factor = config.get('radius_normalizing_factor', 50.0)
         self.speed_normalizing_factor = config.get('speed_normalizing_factor', 10.0)
         # Setup ZeroMQ
@@ -92,71 +92,89 @@ class InferenceServer:
         cos_theta = np.cos(theta)
         return r, sin_theta, cos_theta
     
+    def _update_entity_kinematics(self, entity_df: pd.DataFrame) -> np.ndarray:
+        """
+        Update the kinematics of a specific entity DataFrame.
+        """
+
+        entity_df['vx'] = entity_df['x'].diff()/self.config['dt']
+        entity_df['vy'] = entity_df['y'].diff()/self.config['dt']
+        if len(entity_df) > 1: # Handle the first row
+            entity_df.loc[0, 'vx'] = entity_df.loc[1, 'vx'] 
+            entity_df.loc[0, 'vy'] = entity_df.loc[1, 'vy']
+        else:
+            entity_df.loc[0, 'vx'] = 0.0
+            entity_df.loc[0, 'vy'] = 0.0
+
+        return entity_df[['vx', 'vy']].values
+    
     def process_data(self, data: pd.DataFrame):
         """Process the data."""
         node_data_dict = {}
-        data['frame_id'] = pd.to_numeric(data['frame_id'], downcast='integer')
-        data['track_id'] = pd.to_numeric(data['track_id'], downcast='integer')
-
-        data['frame_id'] = data['frame_id']
-        data['node_type'] = [self.class_map[int(c)] for c in data['class']]
-        data['node_id'] = data['track_id'].astype(str)
+        data['time'] = data['time'].astype(int)
+        data['id'] = data['id'].astype(int)
+        data['x'] = data['x'].astype(float)
+        data['y'] = data['y'].astype(float)
+        data['theta'] = data['theta'].astype(float)
+        data['vehicle_type'] = data['vehicle_type'].astype(int)
         data['cluster'] = data['cluster'].astype(int)
+        data['signal'] = data['signal'].astype(int)
 
-        data.sort_values('frame_id', inplace=True)
+        data.drop(columns=['direction_id', 'maneuver_id', 'region'], inplace=True)
+
+        data['node_type'] = [self.class_map[int(c)] for c in data['vehicle_type']]
+        data['node_id'] = data['id'].astype(str)
+
+        data.sort_values('time', inplace=True)
         signals_col = ["signal_0.0", "signal_1.0", "signal_2.0", "signal_3.0"] # Add more signals later
         one_hot_signal = pd.get_dummies(data['signal'], prefix='signal')
         data[signals_col] = 0
         data[one_hot_signal.columns] = one_hot_signal
         data.loc[:, 'signal'] = data['signal'].astype(int)
 
-        scene = Scene(file_path=None, scene_id=0, signal_file_path=None)
+        scene = Scene(scene_id=0, config=self.config)
 
-        for node_id in pd.unique(data['node_id']):
-            node_df = data[data['node_id'] == node_id]
-            node_type = node_df['node_type'].iloc[0]
-            assert np.all(np.diff(node_df['frame_id']) == 1)
+        for id in pd.unique(data['id']):
+            node_df = data[data['id'] == id]
+            node_type = node_df['vehicle_type'].iloc[0]
+            assert np.all(np.diff(node_df['time']) == 1)
+            vel_array = self._update_entity_kinematics(node_df.copy().reset_index(drop=True))
+            node_df[['vx', 'vy']] = vel_array
+            data.loc[data['id'] == id, ['vx', 'vy']] = vel_array
 
-            node_values = node_df[['pos_x', 'pos_y']].iloc[-1].values
+            node_values = node_df[['x', 'y']].iloc[-1].values
             signal_values = node_df[signals_col].iloc[-1].values
-            node_signal = node_df[signals_col].values
-            theta = node_df['head'].iloc[-1]
+            theta = node_df['theta'].iloc[-1]
             cluster = node_df['cluster'].iloc[-1]
             signal = node_df['signal'].iloc[-1]
 
-            if str(node_id) in self.object_coordinates:
-                delta_x = node_values[0]-self.center_point[0]
-                delta_y = node_values[1]-self.center_point[1]
-                r, sin_theta, cos_theta = self.cartesian_to_polar(delta_x, delta_y)
-                delta_x = (node_values[0]-self.object_coordinates[str(node_id)][0])
-                delta_y = (node_values[1]-self.object_coordinates[str(node_id)][1])
-                speed, tangent_sin, tangent_cos = self.cartesian_to_polar(delta_x, delta_y)
-                speed = speed/0.1
-                node_data_dict[str(node_id)] = {
-                    'x': node_values[0],
-                    'y': node_values[1],
-                    'theta': theta,
-                    'r': r/self.radius_normalizing_factor,
-                    'sin_theta': sin_theta,
-                    'cos_theta': cos_theta,
-                    'speed': speed/self.speed_normalizing_factor,
-                    'tangent_sin': tangent_sin,
-                    'tangent_cos': tangent_cos,
-                    'signal_one_hot': signal_values,
-                    'signal': signal,
-                    'cluster': cluster,
-                    'node_type': node_type,
-                    'theta': theta
-                }
-            self.object_coordinates[str(node_id)] = (node_values[0], node_values[1])
+            r, sin_theta, cos_theta = scene.convert_rectangular_to_polar(node_values)
+            speed = np.sqrt(node_df['vx']**2 + node_df['vy']**2)
+            tangent_sin = np.sin(theta)
+            tangent_cos = np.cos(theta)
+            scene.entity_data[(id, self.timestep)] = {
+                'x': node_values[0],
+                'y': node_values[1],
+                'theta': theta,
+                'r': r/self.radius_normalizing_factor,
+                'sin_theta': sin_theta,
+                'cos_theta': cos_theta,
+                'speed': speed/self.speed_normalizing_factor,
+                'tangent_sin': tangent_sin,
+                'tangent_cos': tangent_cos,
+                'signal_one_hot': signal_values,
+                'signal': signal,
+                'cluster': cluster,
+                'node_type': node_type,
+                'theta': theta
+            }
+        
+        node_dict = data.set_index('id')[['x', 'y', 'cluster']].to_dict("index")
+        scene._create_neighbor_adjacency_dict(self.timestep, node_dict)
+        scene._create_map_adjacency_dict(self.timestep, node_dict)
+        scene._create_signal_adjacency_dict(self.timestep, node_dict)
 
-        return node_data_dict, scene
-    
-    def polar_to_cartesian(self, current_position: Tuple[float, float], speed: float, tangent_sin: float, tangent_cos: float, dt: float) -> Tuple[float, float]:
-        """Convert polar coordinates to cartesian coordinates."""
-        x = current_position[0] + speed * tangent_cos * dt
-        y = current_position[1] + speed * tangent_sin * dt
-        return x, y
+        return scene
     
     def calculate_next_position_linear(self, data, prediction):
             current_position = (data['x'], data['y'])
@@ -185,11 +203,11 @@ class InferenceServer:
             return ' '
         
         data = pd.DataFrame(data)
-        data.columns = ['frame_id', 'track_id', 'pos_x', 'pos_y', 'head', 'class', 'cluster', 'signal', 'direction_id', 'maneuver_id']
+        data.columns = ['time', 'id', 'x', 'y', 'theta', 'vehicle_type', 'cluster', 'signal', 'direction_id', 'maneuver_id', 'region']
 
 
-        node_data_dict, scene = self.process_data(data)
-        predictions = self.inference_model.predict(node_data_dict, scene)
+        scene = self.process_data(data)
+        predictions = self.inference_model.predict(scene)
         timestep_str = str(self.timestep)
         output_json = {timestep_str:{}}
 
